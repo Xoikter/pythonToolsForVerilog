3a6dd39ddc08604be4ba6c48970a3535c8b09eea
shaun <840624732@qq.com>
2023-05-04 22:50:08 +0800

    update code

--- build/build.py
+++ build/build.py
@@ -1,4 +1,4 @@
-build_opts = r"vcs  +acc +vpi -j6   -full64 +v2k -sverilog  -ntb_opts uvm-1.2   +define+DEBUG  -CFLAGS -DVCS -lca -kdb +lint=TFIPC-L -timescale=1ns/1ps -debug_acc+all -debug_region+cell+encrypt -LDFLAGS -rdynamic  -P ${VERDI_HOME}/share/PLI/VCS/LINUX64/novas.tab ${VERDI_HOME}/share/PLI/VCS/LINUX64/pli.a  -f ../filelist/filelist.f " + "../tb/"+ self.filename + "_tb.sv"
+build_opts = r"vcs  +acc +vpi -j16   -full64 +v2k -sverilog  -ntb_opts uvm-1.2   +define+DEBUG  -CFLAGS -DVCS -lca -kdb +lint=TFIPC-L -timescale=1ns/1ps -debug_acc+all -debug_region+cell+encrypt -LDFLAGS -rdynamic  -P ${VERDI_HOME}/share/PLI/VCS/LINUX64/novas.tab ${VERDI_HOME}/share/PLI/VCS/LINUX64/pli.a  -f ../filelist/filelist.f " + "../tb/"+ self.filename + "_tb.sv"
 
 lists = {
     "base_build":{"build_opts":build_opts}
